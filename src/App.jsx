df2fe1d5f628040ccde139d40178509a2e6e6de4
Eden Kamieniecki <foxiakkomunista@gmail.com>
2023-11-27 17:34:44 +0100

    Added the fully functional description

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { ListElement } from "./components/ListElement";
+import React, { useState, useRef } from "react";
+import ListElement from "./components/ListElement";
 import './App.css'
 
 const useLocalStorage = (key, initialValue) => {
@@ -18,17 +18,27 @@ const useLocalStorage = (key, initialValue) => {
 
 function App() {
   const [tasks, setTasks] = useLocalStorage("tasks", []);
+  const [describe, setDescribe] = useLocalStorage("descriptions", []);
+
   const taskInputRef = useRef();
+  const descriptionInputRef = useRef();
 
   const addTask = (event) => {
     event.preventDefault();
     const newTask = taskInputRef.current.value.trim();
+    let newDescription = descriptionInputRef.current.value.trim();
 
     if (newTask) {
       const updatedTasks = [...tasks, newTask];
       setTasks(updatedTasks);
       taskInputRef.current.value = "";
     }
+    if (!newDescription) {
+      newDescription = "No description given."
+    }
+    const updatedDescriptions = [...describe, newDescription];
+    setDescribe(updatedDescriptions);
+    descriptionInputRef.current.value = "";
   };
 
   return (
@@ -39,13 +49,14 @@ function App() {
       ) : (
         <ul>
           {tasks.map((task, index) => (
-            <ListElement index={index} task={task} update={setTasks} tasks={tasks}/>
+            <ListElement index={index} task={task} updateName={setTasks} updateDesc={setDescribe} tasks={tasks} description={describe}/>
           ))}
         </ul>
       )}
       <form onSubmit={addTask}>
-        <label htmlFor="taskInput">Task:</label>
-        <input type="text" id="taskInput" ref={taskInputRef} />
+        <label htmlFor="taskInput">Task:<br/></label>
+        <input type="text" id="taskInput" ref={taskInputRef} placeholder="Name of the task" size="51" max="40" min="3"/><br/>
+        <textarea ref={descriptionInputRef} placeholder="Description" rows="4" cols="50"/><br/>
         <button type="submit">Add Task</button>
       </form>
     </div>
--- src/components/ListElement.jsx
+++ src/components/ListElement.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
+
 function ListElement(props){
+  const [show, changeShow] = useState(false);
+  function turnOff(){
+    changeShow(false);
+  }
+  function turnOn(){
+    changeShow(true);
+  }
     const editTask = (index) => {
         const updatedTasks = [...props.tasks];
         const newTask = prompt("What should the task be renamed to?", updatedTasks[index]);
-        if(newTask !== null && newTask != ""){
+        if(newTask !== null && newTask !== ""){
           updatedTasks[index] = newTask;
-          props.update(updatedTasks);
+          props.updateName(updatedTasks);
         }
       };
       const deleteTask = (index) => {
         if(window.confirm("Are you sure?")){
           const updatedTasks = [...props.tasks];
           updatedTasks.splice(index, 1);
-          props.update(updatedTasks);
+          props.updateName(updatedTasks);
+          const updatedDesc = [...props.description];
+          updatedDesc.splice(index, 1);
+          props.updateDesc(updatedDesc);
         }
       };
-    return(<li key={props.index}>{props.task}<span className="showMore">Show More</span></li>);
+    return(<li key={props.index}><h3>{props.task}</h3>
+      {(show && <><span className="show" onClick={turnOff}><sup><sub>Show Less</sub></sup></span>
+      <ul className="description"><li>{props.description[props.index]}<br/><br/>
+        <span className="delete" onClick={() => deleteTask(props.index)}>Delete</span><span className="edit" onClick={() => editTask(props.index)}>Edit</span>
+        </li></ul></>) || <span className="show" onClick={turnOn}><sup><sub>Show More</sub></sup></span>}
+    </li>);
 }
-/*<span className="delete" onClick={() => deleteTask(index)}>Delete</span>     <span className="edit" onClick={() => editTask(index)}>Edit</span>*/
-export default ListElement;
+export default ListElement; 
